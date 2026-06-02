a43d6faf05546c7578812b4f95e979d1d5db0235
Beata von Grothusen <beatavg@Beatas-MacBook-Air.local>
2026-06-02 12:56:31 +0200

    added basic FastApi endpoints

---
+++ main.py
@@ -0,0 +1,41 @@
+from decimal import Decimal
+
+from app.services.fibonacci import fibonacci
+from app.services.factorial import factorial
+from app.services.loan_repayment import loan_repayment
+from fastapi import FastAPI
+
+# python -m uvicorn main:app --reload
+
+# http://127.0.0.1:8000/docs
+app = FastAPI()
+
+# http://127.0.0.1:8000/
+@app.get("/")
+def root():
+    return {"message": "Calculation Service"}
+
+# http://127.0.0.1:8000/fibonacci/10
+@app.get("/fibonacci/{n}")
+def get_fibonacci(n: int):
+    return {"result": fibonacci(n)}
+
+# http://127.0.0.1:8000/factorial/5
+@app.get("/factorial/{n}")
+def get_factorial(n: int):
+    return {"result": factorial(n)}
+
+# http://127.0.0.1:8000/loan_repayment?principal=12000&annual_rate=0&months=12
+@app.get("/loan_repayment")
+def get_loan(
+    principal: Decimal,
+    annual_rate: Decimal,
+    months: int
+):
+    return {
+        "monthly_payment": loan_repayment(
+            principal,
+            annual_rate,
+            months
+        )
+    }
